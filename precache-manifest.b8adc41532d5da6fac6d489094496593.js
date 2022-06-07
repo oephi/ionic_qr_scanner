cd5239041d04d5cd3e34eeb7906cad0daff35add
oephi <r.sickle@protonmail.com>
2022-06-07 10:49:58 +1000

    Updates

---
+++ precache-manifest.b8adc41532d5da6fac6d489094496593.js
@@ -0,0 +1,450 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aed7b69032f4879a23519bfeebe2e058",
+    "url": "/ionic_qr_scanner/index.html"
+  },
+  {
+    "revision": "ff7f00de7921fcbb6464",
+    "url": "/ionic_qr_scanner/static/css/49.a72581b3.chunk.css"
+  },
+  {
+    "revision": "1ed7ee2166ad85698c6b",
+    "url": "/ionic_qr_scanner/static/css/main.31c35379.chunk.css"
+  },
+  {
+    "revision": "94f08ffbac3c7a30f690",
+    "url": "/ionic_qr_scanner/static/js/0.1e4e980c.chunk.js"
+  },
+  {
+    "revision": "c293a5f7f0300aeb5d65",
+    "url": "/ionic_qr_scanner/static/js/1.7483c256.chunk.js"
+  },
+  {
+    "revision": "00f37851bcac8b46ee5b",
+    "url": "/ionic_qr_scanner/static/js/100.3bbd4757.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/ionic_qr_scanner/static/js/100.3bbd4757.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da5f859dfd9e5f806d0e",
+    "url": "/ionic_qr_scanner/static/js/101.dccd3ce6.chunk.js"
+  },
+  {
+    "revision": "589fdc1831ae1271db21",
+    "url": "/ionic_qr_scanner/static/js/102.71a32376.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/ionic_qr_scanner/static/js/102.71a32376.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "896694f632b940d67799",
+    "url": "/ionic_qr_scanner/static/js/2.bde2bc97.chunk.js"
+  },
+  {
+    "revision": "4247436298af10b3b52f",
+    "url": "/ionic_qr_scanner/static/js/3.39953c98.chunk.js"
+  },
+  {
+    "revision": "2760deaf399ed76260c3",
+    "url": "/ionic_qr_scanner/static/js/4.c547f7da.chunk.js"
+  },
+  {
+    "revision": "ff7f00de7921fcbb6464",
+    "url": "/ionic_qr_scanner/static/js/49.2e3f1eb6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ionic_qr_scanner/static/js/49.2e3f1eb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "418360837870dab87bce",
+    "url": "/ionic_qr_scanner/static/js/5.619b47d4.chunk.js"
+  },
+  {
+    "revision": "3031d7b7ed99ef4c486b",
+    "url": "/ionic_qr_scanner/static/js/50.8f0801dd.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/ionic_qr_scanner/static/js/50.8f0801dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09dd6f3d70dee7f2d8d6",
+    "url": "/ionic_qr_scanner/static/js/51.9672b8b5.chunk.js"
+  },
+  {
+    "revision": "ea1b6d09416edbea6b63",
+    "url": "/ionic_qr_scanner/static/js/52.21f835ee.chunk.js"
+  },
+  {
+    "revision": "8d82de629d1c1b861652",
+    "url": "/ionic_qr_scanner/static/js/53.baaec1b6.chunk.js"
+  },
+  {
+    "revision": "d81480df6b5465772d43",
+    "url": "/ionic_qr_scanner/static/js/54.4833109c.chunk.js"
+  },
+  {
+    "revision": "bb9c5120a4af9e6040cb",
+    "url": "/ionic_qr_scanner/static/js/55.5daa8c71.chunk.js"
+  },
+  {
+    "revision": "d886bd46a1121df55bd0",
+    "url": "/ionic_qr_scanner/static/js/56.c7d1f4dd.chunk.js"
+  },
+  {
+    "revision": "342c0fd1e1fdda0764a4",
+    "url": "/ionic_qr_scanner/static/js/57.1a6023bb.chunk.js"
+  },
+  {
+    "revision": "161a4ee69814158f8d53",
+    "url": "/ionic_qr_scanner/static/js/58.034a0825.chunk.js"
+  },
+  {
+    "revision": "976227087d8ceb3fb06a",
+    "url": "/ionic_qr_scanner/static/js/59.a86960ff.chunk.js"
+  },
+  {
+    "revision": "0536ebe563b875f72c51",
+    "url": "/ionic_qr_scanner/static/js/60.1f3de912.chunk.js"
+  },
+  {
+    "revision": "c6a859ac511e50ecd859",
+    "url": "/ionic_qr_scanner/static/js/61.85b839e7.chunk.js"
+  },
+  {
+    "revision": "ed31e6e3eb8c0589f74a",
+    "url": "/ionic_qr_scanner/static/js/62.e73c9dc3.chunk.js"
+  },
+  {
+    "revision": "fe868a1c363ccb34656d",
+    "url": "/ionic_qr_scanner/static/js/63.f77973c9.chunk.js"
+  },
+  {
+    "revision": "df3c49765801a1dc1a7f",
+    "url": "/ionic_qr_scanner/static/js/64.1fa9f5bc.chunk.js"
+  },
+  {
+    "revision": "a5f2b2643d1fcab48ea6",
+    "url": "/ionic_qr_scanner/static/js/65.cdef1695.chunk.js"
+  },
+  {
+    "revision": "bcaeab4a93701b8ca556",
+    "url": "/ionic_qr_scanner/static/js/66.47298170.chunk.js"
+  },
+  {
+    "revision": "37b11b61bc3022db6a39",
+    "url": "/ionic_qr_scanner/static/js/67.797c3f9f.chunk.js"
+  },
+  {
+    "revision": "2b3c7857402e719b6b97",
+    "url": "/ionic_qr_scanner/static/js/68.2dedfe48.chunk.js"
+  },
+  {
+    "revision": "f947921ca819643ce028",
+    "url": "/ionic_qr_scanner/static/js/69.510207cc.chunk.js"
+  },
+  {
+    "revision": "15b66b485b60fdc389eb",
+    "url": "/ionic_qr_scanner/static/js/70.3044aee7.chunk.js"
+  },
+  {
+    "revision": "6ce84067e9efd87338ad",
+    "url": "/ionic_qr_scanner/static/js/71.7afa1760.chunk.js"
+  },
+  {
+    "revision": "f8de70c49f15c98b9368",
+    "url": "/ionic_qr_scanner/static/js/72.f0035780.chunk.js"
+  },
+  {
+    "revision": "48b648307d1079dd8ceb",
+    "url": "/ionic_qr_scanner/static/js/73.545e43f3.chunk.js"
+  },
+  {
+    "revision": "5f7622b87fc668918c2d",
+    "url": "/ionic_qr_scanner/static/js/74.e36f7f76.chunk.js"
+  },
+  {
+    "revision": "ad0f578881a9d6f7ecf2",
+    "url": "/ionic_qr_scanner/static/js/75.d984d2e5.chunk.js"
+  },
+  {
+    "revision": "cadc0fa63df39dbbc403",
+    "url": "/ionic_qr_scanner/static/js/76.493a76f8.chunk.js"
+  },
+  {
+    "revision": "d8d27491d56bf86c841c",
+    "url": "/ionic_qr_scanner/static/js/77.033731e7.chunk.js"
+  },
+  {
+    "revision": "f7e1d77f835f9e364834",
+    "url": "/ionic_qr_scanner/static/js/78.e38fc4fd.chunk.js"
+  },
+  {
+    "revision": "b8115b01ef7aae5fb1ca",
+    "url": "/ionic_qr_scanner/static/js/79.c4c4a985.chunk.js"
+  },
+  {
+    "revision": "05366c2a80180cc3f3f4",
+    "url": "/ionic_qr_scanner/static/js/80.eb20897a.chunk.js"
+  },
+  {
+    "revision": "19cf095763b99045cf2d",
+    "url": "/ionic_qr_scanner/static/js/81.6c96af88.chunk.js"
+  },
+  {
+    "revision": "84d6917275e24eb0ae30",
+    "url": "/ionic_qr_scanner/static/js/82.b7678ae2.chunk.js"
+  },
+  {
+    "revision": "4c6029b459de87709035",
+    "url": "/ionic_qr_scanner/static/js/83.4e668209.chunk.js"
+  },
+  {
+    "revision": "1219f06fcdf80bbdde9c",
+    "url": "/ionic_qr_scanner/static/js/84.522593dd.chunk.js"
+  },
+  {
+    "revision": "2981a4af0ae1db251b18",
+    "url": "/ionic_qr_scanner/static/js/85.e069a247.chunk.js"
+  },
+  {
+    "revision": "279d2d4f4f28ff2e39d7",
+    "url": "/ionic_qr_scanner/static/js/86.874f10fe.chunk.js"
+  },
+  {
+    "revision": "1425119a19a838c402d1",
+    "url": "/ionic_qr_scanner/static/js/87.046eb59b.chunk.js"
+  },
+  {
+    "revision": "a0a57c6e92588119cc25",
+    "url": "/ionic_qr_scanner/static/js/88.bd0c5781.chunk.js"
+  },
+  {
+    "revision": "f62a5abfea00a8c45350",
+    "url": "/ionic_qr_scanner/static/js/89.7f051803.chunk.js"
+  },
+  {
+    "revision": "f767d291a1da5198852f",
+    "url": "/ionic_qr_scanner/static/js/90.78b5b8bf.chunk.js"
+  },
+  {
+    "revision": "700936cd9dc5a01467a8",
+    "url": "/ionic_qr_scanner/static/js/91.99ed6fff.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/ionic_qr_scanner/static/js/91.99ed6fff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "437b7d49023f1d1f2968",
+    "url": "/ionic_qr_scanner/static/js/92.fb46b0c1.chunk.js"
+  },
+  {
+    "revision": "931bb97bf8ecce9f394f",
+    "url": "/ionic_qr_scanner/static/js/93.47d3ac30.chunk.js"
+  },
+  {
+    "revision": "9c873bd0c01b0f4f2685",
+    "url": "/ionic_qr_scanner/static/js/94.34bf988f.chunk.js"
+  },
+  {
+    "revision": "992e9b91bc160f67024c",
+    "url": "/ionic_qr_scanner/static/js/95.1320c635.chunk.js"
+  },
+  {
+    "revision": "c0219900839118fd5f34",
+    "url": "/ionic_qr_scanner/static/js/96.e21a26ee.chunk.js"
+  },
+  {
+    "revision": "feb59083003df50bccee",
+    "url": "/ionic_qr_scanner/static/js/97.40ff7606.chunk.js"
+  },
+  {
+    "revision": "6338b1d69ee481376d46",
+    "url": "/ionic_qr_scanner/static/js/98.65880669.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/ionic_qr_scanner/static/js/98.65880669.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2320d127b39eb04ec32c",
+    "url": "/ionic_qr_scanner/static/js/99.552d468b.chunk.js"
+  },
+  {
+    "revision": "1ed7ee2166ad85698c6b",
+    "url": "/ionic_qr_scanner/static/js/main.cba74c62.chunk.js"
+  },
+  {
+    "revision": "7777c6030656b7211a87",
+    "url": "/ionic_qr_scanner/static/js/runtime-main.686d228e.js"
+  },
+  {
+    "revision": "e1a07fafc24eb7ef080e",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-avatar_3-ios-entry-js.780fe2ed.chunk.js"
+  },
+  {
+    "revision": "a770d9b8931037461fe5",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-avatar_3-md-entry-js.a6bc42b0.chunk.js"
+  },
+  {
+    "revision": "4145ec4a87f003865c2e",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-back-button-ios-entry-js.c5f214e7.chunk.js"
+  },
+  {
+    "revision": "583c063b1ab30769294e",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-back-button-md-entry-js.8d72e949.chunk.js"
+  },
+  {
+    "revision": "7c62489956d8abe8e792",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-backdrop-ios-entry-js.4e16a9b9.chunk.js"
+  },
+  {
+    "revision": "c9ef832a14d92d830261",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-backdrop-md-entry-js.080661bb.chunk.js"
+  },
+  {
+    "revision": "5f87813333957bc39365",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-card_5-ios-entry-js.b26e4d59.chunk.js"
+  },
+  {
+    "revision": "d0bf4e3381358e54a299",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-card_5-md-entry-js.3d21877f.chunk.js"
+  },
+  {
+    "revision": "857b5b46b6094f1fdac4",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-checkbox-ios-entry-js.885875c3.chunk.js"
+  },
+  {
+    "revision": "d6283cb15ae5689209fa",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-checkbox-md-entry-js.184bb661.chunk.js"
+  },
+  {
+    "revision": "8f8c7d6d1a09e2515621",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-chip-ios-entry-js.d7afcba8.chunk.js"
+  },
+  {
+    "revision": "baaad4c1d0f4a4dfd2f9",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-chip-md-entry-js.8d71d7cc.chunk.js"
+  },
+  {
+    "revision": "e37d3b4b7f5215eed76e",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-col_3-entry-js.7b9d1e67.chunk.js"
+  },
+  {
+    "revision": "1be8f9fc9da3ab2fb1c2",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-img-entry-js.369cfcf5.chunk.js"
+  },
+  {
+    "revision": "ef67197bbbc85993aa6c",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-infinite-scroll_2-ios-entry-js.9db9b2ba.chunk.js"
+  },
+  {
+    "revision": "b608edf2c218cbbb2660",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-infinite-scroll_2-md-entry-js.e4d10655.chunk.js"
+  },
+  {
+    "revision": "1452af50a2f47a85fb8a",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-input-ios-entry-js.fd5b48d2.chunk.js"
+  },
+  {
+    "revision": "ceaae0887c1ac10bf7a9",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-input-md-entry-js.2bc5b259.chunk.js"
+  },
+  {
+    "revision": "90b0955f1fbdcdbff9cd",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-loading-ios-entry-js.f486c36e.chunk.js"
+  },
+  {
+    "revision": "8da4e0ccbb283d61b3d4",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-loading-md-entry-js.efb0a77e.chunk.js"
+  },
+  {
+    "revision": "55b0470d7653fc6ce9d1",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-popover-ios-entry-js.0f6f42ab.chunk.js"
+  },
+  {
+    "revision": "3a10b1a032d0058de35e",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-popover-md-entry-js.12618c1e.chunk.js"
+  },
+  {
+    "revision": "6ca99515c099eb3353ba",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-progress-bar-ios-entry-js.a5a8b485.chunk.js"
+  },
+  {
+    "revision": "9fc3e29c3456d9d32d0f",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-progress-bar-md-entry-js.f4168224.chunk.js"
+  },
+  {
+    "revision": "6b8d7426d4d583b94fd6",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-radio_2-ios-entry-js.6b2001e8.chunk.js"
+  },
+  {
+    "revision": "d2707b40949ce195c04f",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-radio_2-md-entry-js.bd7dd29a.chunk.js"
+  },
+  {
+    "revision": "cdc83b087eac62f4ebea",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-reorder_2-ios-entry-js.d991258d.chunk.js"
+  },
+  {
+    "revision": "9e29757ceea9170640a1",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-reorder_2-md-entry-js.213fb686.chunk.js"
+  },
+  {
+    "revision": "9fb736f26c764efe811f",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-ripple-effect-entry-js.0e626143.chunk.js"
+  },
+  {
+    "revision": "b47657d99e42f0e0af45",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-spinner-entry-js.ed757883.chunk.js"
+  },
+  {
+    "revision": "4d1a4db90789b2464144",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-split-pane-ios-entry-js.d9316de9.chunk.js"
+  },
+  {
+    "revision": "8a9feda1ebadce0b5fe2",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-split-pane-md-entry-js.4e321044.chunk.js"
+  },
+  {
+    "revision": "f45ce19ebd851ddf72d5",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-tab-bar_2-ios-entry-js.4a942914.chunk.js"
+  },
+  {
+    "revision": "e5f9fd858ab4477817b2",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-tab-bar_2-md-entry-js.e80310f7.chunk.js"
+  },
+  {
+    "revision": "7b690a896a87894b228b",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-tab_2-entry-js.06519eae.chunk.js"
+  },
+  {
+    "revision": "513011ec728547164759",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-text-entry-js.6d8dd90d.chunk.js"
+  },
+  {
+    "revision": "07dc9eb2a97d985d129d",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-textarea-ios-entry-js.ae3d7cc6.chunk.js"
+  },
+  {
+    "revision": "628736f8e045377a3647",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-textarea-md-entry-js.423f81a9.chunk.js"
+  },
+  {
+    "revision": "1ba37f14c170264896d6",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-toggle-ios-entry-js.6ba0538f.chunk.js"
+  },
+  {
+    "revision": "83cddba0edc5c15d1e7f",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-toggle-md-entry-js.12267d0c.chunk.js"
+  },
+  {
+    "revision": "4993f502f0d8c691ef3c",
+    "url": "/ionic_qr_scanner/static/js/stencil-ion-virtual-scroll-entry-js.91403d3f.chunk.js"
+  }
+]);
